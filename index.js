adc3a4f2f6526c4a7bb05930cb3355439b4b2f31
JustJJh <148192129+JustJJh@users.noreply.github.com>
2024-02-01 21:14:50 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+import {initializeApp } from "https://www.gstatic.com/firebase.js/9.15.0/firebase-app.js"
+import { getDatabase,ref} from "https://gstatic.com/firebase.js/9.15.0/firebase-database.js"
+import { generateBarcode } from "thanks_for_purchase.html"
+
+const appSettings = {
+    databaseURL: "https://devops-website-f2268-default-rtdb.asia-southeast1.firebasedatabase.app/"
+}
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const barcodeInDB = ref(database, "Barcode")
