56ffadbd07511b255c3db824d9b6d545839440a7
JustJJh <148192129+JustJJh@users.noreply.github.com>
2024-02-01 21:33:51 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import {initializeApp } from "https://www.gstatic.com/firebase.js/9.15.0/firebase-app.js"
-import { getDatabase,ref} from "https://gstatic.com/firebase.js/9.15.0/firebase-database.js"
-import { generateBarcode } from "thanks_for_purchase.html"
+import { getDatabase,ref,push} from "https://gstatic.com/firebase.js/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://devops-website-f2268-default-rtdb.asia-southeast1.firebasedatabase.app/"
@@ -9,3 +8,10 @@ const app = initializeApp(appSettings)
 const database = getDatabase(app)
 const barcodeInDB = ref(database, "Barcode")
 
+const drinkType = document.getElementByID("value")
+const addButton = document.getElementById("add_button")
+
+addButton.addEventListener("click",function() {
+   let drinksVal = drinkType.value
+    push(barcodeInDB, drinksVal)
+    console.log('${drinksVal} added to database')
